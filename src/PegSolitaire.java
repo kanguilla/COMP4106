5a932672904d8da4fb1d34fd786fdca9e5cbf551
Khalil <khalil.va@gmail.com>
2018-02-26 10:14:56 -0500

    goo

--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -3,18 +3,18 @@ public class PegSolitaire {
 	
 	public static void main (String[] args){
 		
-		Astar<PegState> ex = new Astar<PegState>();
-		ex.addHeuristic(new Central());
+		DFS<PegState> ex = new DFS<PegState>();
+		//ex.addHeuristic(new Central());
 		//ex.addHeuristic(new AvoidIsolated());
-		ex.addHeuristic(new ClearEdges());
+		//ex.addHeuristic(new ClearEdges());
 		
 		PegState base = new PegState(0, 0);
 		
-		base.setBoard(Layouts.euro);
+		base.setBoard(Layouts.english);
 		//base.setMoves(Layouts.triangleMoves);
 		//base.triangle = true;
 		
-		ex.verbose(1);
+		//ex.verbose(1);
 		//ex.slow();
 		ex.setBase(base);
 		ex.setSolutionCount(1);
